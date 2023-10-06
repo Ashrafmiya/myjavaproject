90d1aca503809d819303f3032e61124af7e71102
Ashrafmiya <ashrafmiya4@gmail.com>
2023-10-06 12:04:23 +0530

    without error

--- myjava.java
+++ myjava.java
@@ -2,7 +2,7 @@ class Simple{
 	
     public static void main(String args[]){  
 	
-     System.out.println("Hello Fortune");  
+     System.out.println("Hello Friends have a good day");  
 	
     }  
 	
